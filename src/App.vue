4fe10b29742e972284bd2dc41fe14bf4ac67b80c
kamto <107313631+kamto-s@users.noreply.github.com>
2025-12-24 11:25:46 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <p>Hello World!</p>
+  <hr />
+  <Student @response="showData" studentName="Andy" />
+  <Student @response="showData" studentName="Bob" />
+  <Student @response="showData" studentName="Charlie" />
+  <p>Student yan dipilih adalah : {{ selectedStudent }}</p>
+</template>
+
+<script>
+import Student from "./components/Student.vue";
+export default {
+  components: {
+    Student,
+  },
+  data() {
+    return {
+      selectedStudent: "",
+    };
+  },
+  methods: {
+    showData(student) {
+      this.selectedStudent = student;
+    },
+  },
+};
+</script>
---
+++ src/components/Student.vue
@@ -0,0 +1,19 @@
+<template>
+  <p>
+    Component Student <strong>{{ studentName }} <button @click="showData">click me</button></strong>
+  </p>
+</template>
+
+<script>
+export default {
+  props: {
+    studentName: String,
+  },
+  emits: ["response"],
+  methods: {
+    showData() {
+      this.$emit("response", this.studentName);
+    },
+  },
+};
+</script>
